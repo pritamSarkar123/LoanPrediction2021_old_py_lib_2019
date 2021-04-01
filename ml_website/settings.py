1cb184cd3a38a76cf2c90ef8b8908598e296f4d3
pritamSarkar123 <pritamsarkar84208220@gmail.com>
2021-04-01 21:47:58 +0530

    db changed to sqllite 3

--- ml_website/settings.py
+++ ml_website/settings.py
@@ -76,13 +76,17 @@ WSGI_APPLICATION = 'ml_website.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'newloanpredictordb',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'pritams1',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'newloanpredictordb',
-        'USER': 'postgres',
-        'PASSWORD': 'pritams1',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
